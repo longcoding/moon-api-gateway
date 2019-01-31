02625cb3dee64c86e846db70499eb1f524d03046
longcoding <longcoding@gmail.com>
2019-01-31 22:46:32 +0900

    Added code comment for internal apis

--- src/main/java/com/longcoding/undefined/controllers/internal/ApiController.java
+++ src/main/java/com/longcoding/undefined/controllers/internal/ApiController.java
@@ -8,10 +8,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 /**
- * Created by longcoding on 19. 1. 17..
+ * Internal API for managing API specifications
+ * Any node in the cluster can receive these requests.
+ * When it receives the request, it sends the event to other nodes.
+ *
+ * The model object for the client request differs from the model object for the service layer.
+ *
+ * @author longcoding
  */
 
 @Slf4j
@@ -22,15 +26,43 @@ public class ApiController {
     @Autowired
     ApiService apiService;
 
+    /**
+     * Register a new API specification. This job does not load into the cache.
+     * When a new API specification request is received, it serves to issue events to other nodes, including itself.
+     *
+     * @param enrollApi API specification model for client requests.
+     * @return API specification model for internal processing.
+     */
     @RequestMapping(method = RequestMethod.POST)
-    public ApiInfo createApp(@RequestBody EnrollApi enrollApi) { return apiService.createOrModifyApi(SyncType.CREATE,  enrollApi); }
+    public ApiInfo createApi(@RequestBody EnrollApi enrollApi) { return apiService.createOrModifyApi(SyncType.CREATE,  enrollApi); }
 
+    /**
+     * Delete the existing api specification. This action does not directly apply to the cache.
+     * When a new api specification update request is received, it is responsible for issuing an event to other nodes including itself.
+     *
+     * @param apiId Application Id.
+     * @return Returns success or failure.
+     */
     @RequestMapping(value = "{apiId}", method = RequestMethod.DELETE)
-    public boolean deleteApp(@PathVariable String apiId) { return apiService.deleteApi(apiId); }
+    public boolean deleteApi(@PathVariable String apiId) { return apiService.deleteApi(apiId); }
 
+    /**
+     * Update existing api specifications. This action does not directly apply to the cache.
+     * When a new api specification update request is received, it is responsible for issuing an event to other nodes including itself.
+     *
+     * @param apiId Application Id.
+     * @param enrollApi API specification model for client requests.
+     * @return API specification model for internal processing.
+     */
     @RequestMapping(value = "{apiId}", method = RequestMethod.PUT)
     public ApiInfo updateApi(@PathVariable String apiId, @RequestBody EnrollApi enrollApi) { return apiService.createOrModifyApi(SyncType.UPDATE, enrollApi); }
 
+    /**
+     * Query the corresponding api specification.
+     *
+     * @param apiId Application Id.
+     * @return API specification model for internal processing.
+     */
     @RequestMapping(value = "{apiId}", method = RequestMethod.GET)
     public ApiInfo getApi(@PathVariable String apiId) { return apiService.selectApi(apiId); }
 
--- src/main/java/com/longcoding/undefined/controllers/internal/AppController.java
+++ src/main/java/com/longcoding/undefined/controllers/internal/AppController.java
@@ -9,7 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 /**
- * Created by longcoding on 19. 1. 7..
+ * Internal API for managing Application information.
+ * Any node in the cluster can receive these requests.
+ * When it receives the request, it sends the event to other nodes.
+ *
+ * The model object for the client request differs from the model object for the service layer.
+ *
+ * @author longcoding
  */
 
 @Slf4j
@@ -20,28 +26,79 @@ public class AppController {
     @Autowired
     AppService appService;
 
+    /**
+     * Register a new application. This action does not directly apply to the cache.
+     * When a new application registration request is received, it issues an event to other nodes including itself.
+     *
+     * @param enrollApp Application information model for client requests.
+     * @return An application information model for internal processing.
+     */
     @RequestMapping(method = RequestMethod.POST)
     public AppInfo createApp(@RequestBody EnrollApp enrollApp) { return appService.createApp(enrollApp); }
 
+    /**
+     * Delete existing application information. This action does not directly apply to the cache.
+     * When a new application update request is received, it issues an event to other nodes including itself.
+     *
+     * @param appId Application Id.
+     * @return Returns success or failure.
+     */
     @RequestMapping(value = "{appId}", method = RequestMethod.DELETE)
     public boolean deleteApp(@PathVariable String appId) {
         return appService.deleteApp(appId);
     }
 
+    /**
+     * View existing application information.
+     *
+     * @param appId Application Id.
+     * @return An application information model for internal processing.
+     */
     @RequestMapping(value = "{appId}", method = RequestMethod.GET)
     public AppInfo getAppInfo(@PathVariable String appId) { return appService.getAppInfo(appId); }
 
+    /**
+     * Destroys the api key of the corresponding application.
+     * When a new application update request is received, it issues an event to other nodes including itself.
+     *
+     * @param appId Application Id.
+     * @return Returns success or failure.
+     */
     @RequestMapping(value = "{appId}/apiKey", method = RequestMethod.DELETE)
     public boolean expireApiKey(@PathVariable String appId) {
         return appService.expireApiKey(appId);
     }
 
+    /**
+     * Regenerate the api key of the corresponding application.
+     * This includes creating a new UUID for the api key.
+     * When a new application update request is received, it issues an event to other nodes including itself.
+     *
+     * @param appId Application Id.
+     * @return An application information model for internal processing.
+     */
     @RequestMapping(value = "{appId}/apiKey", method = RequestMethod.PUT)
     public AppInfo refreshApiKey(@PathVariable String appId) { return appService.refreshApiKey(appId); }
 
+    /**
+     * Add a new whitelist ip to the application.
+     * When a new application update request is received, it issues an event to other nodes including itself.
+     * When a new whitelist ip is actually registered on the node, the client can call api with that ip.
+     *
+     * @param enrollWhitelistIps The application whitelist model for client requests. It contains an appId.
+     * @return Returns success or failure.
+     */
     @RequestMapping(value = "{appId}/whitelist", method = RequestMethod.POST)
     public boolean addWhitelistIps(@RequestBody EnrollWhitelistIp enrollWhitelistIps) { return appService.addWhiteIps(enrollWhitelistIps); }
 
+    /**
+     * Delete the new whitelist ip in the application.
+     * When a new application update request is received, it issues an event to other nodes including itself.
+     * If the deleted whitelist ip is actually applied to the node cache, the client will not be able to call api with that ip.
+     *
+     * @param enrollWhitelistIps The application whitelist model for client requests. It contains an appId.
+     * @return Returns success or failure.
+     */
     @RequestMapping(value = "{appId}/whitelist", method = RequestMethod.DELETE)
     public boolean removeWhitelistIps(@RequestBody EnrollWhitelistIp enrollWhitelistIps) { return appService.removeWhiteIps(enrollWhitelistIps); }
 
