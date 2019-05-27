2fe7e52b9ab449255e2cbaded862d57f8311b9c5
longcoding <longcoding@gmail.com>
2019-05-27 10:45:16 +0900

    Rename the log property

--- src/main/java/com/longcoding/moon/helpers/ApplicationLogFormatter.java
+++ src/main/java/com/longcoding/moon/helpers/ApplicationLogFormatter.java
@@ -32,8 +32,8 @@ public final class ApplicationLogFormatter {
         accessLog.put("clinetIp", requestInfo.getClientIp());
         accessLog.put("statusCode", requestInfo.getResponseStatusCode());
         accessLog.put("errorCode", requestInfo.getErrorCode());
-        accessLog.put("getServicePath", requestInfo.getServicePath());
-        accessLog.put("getRequestPath", requestInfo.getRequestPath());
+        accessLog.put("servicePath", requestInfo.getServicePath());
+        accessLog.put("requestPath", requestInfo.getRequestPath());
         accessLog.put("elapseTime", System.currentTimeMillis() - requestInfo.getRequestStartTime());
         accessLog.put("requestDataSize", String.valueOf(requestInfo.getRequestDataSize()));
         accessLog.put("acceptHostIp", requestInfo.getAcceptHostIp());
